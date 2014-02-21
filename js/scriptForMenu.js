f34663a7a55ca2a321963ef1fcb7b35977dac34e
tonythomas01 <01tonythomas@gmail.com>
2014-02-21 20:29:31 +0530

    deleted everyting

--- js/scriptForMenu.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() {
-    $("#menu").accordion();
-}); 
--- js/showmeHistory.js
+++
@@ -1,7 +0,0 @@
-window.onload=function()	{
-var historyClicked=document.getElementById('showHistory');
-historyClicked.onclick=function()	{
-alert(localStorage.history);
-}
-
-}
